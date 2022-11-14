caa55a29790a1d79b0eff572c0aa77ef8fd8fb30
Sammirah <103369979+Sammirah@users.noreply.github.com>
2022-11-14 18:10:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
-//Criar uma referência para tela
-
-//Dê altura e largura específicas para a imagem do carro
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
 
 backgroundImage = "parkingLot.jpg";
 greencarImage = "car2.png";
 
-//Defina a posição inicial para uma imagem de carro.
+greencar_width = 75;
+greencar_height = 100;
+
+greencar_x = 5;
+greencar_y = 225;
 
 function add() {
-	//carregar carro e imagens de fundo na tela.
+	background_imgTag = new Image();
+	background_imgTag.onload = uploadBackground;
+	background_imgTag.src = backgroundImage
+
+	greencar_imgTag = new Image();
+	greencar_imgTag.onload = uploadGreenCar;
+	greencar_imgTag.src = greencarImage 
 }
 
 function uploadBackground() {
-	//Defina a função ‘uploadBackground’
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
 
 }
 
 function uploadGreenCar() {
-	//Defina a função ‘uploadGreenCar’.
+	ctx.drawImage(greencar_imgTag, greencar_x, greencar_y, greencar_width, greencar_height);
 
 	
 }
+
 
 
 window.addEventListener("keydown", myKeyDown);
@@ -58,20 +68,39 @@ function myKeyDown(e)
 
 function up()
 {
-	//Definir função para mover o carro para cima
+	if(greencar_y >=0) 
+	{
+		greencar_y = greencar_y - 10;
+		uploadBackground();
+		uploadGreenCar();
+	}
 }
 
 function down()
 {
-	//Definir função para mover o carro para baixo
+	if(greencar_y <=300) 
+	{
+		greencar_y = greencar_y + 10;
+		uploadBackground();
+		uploadGreenCar();
+	}
 }
 
 function left()
 {
-	//Definir função para mover o carro para o lado esquerdo
-}
+	if(greencar_x >=0) 
+	{
+		greencar_x = greencar_x - 10;
+		uploadBackground();
+		uploadGreenCar();
+	}}
 
 function right()
 {
-	//Definir função para mover o lado direito do carro
+	if(greencar_x <=725) 
+	{
+		greencar_x = greencar_x + 10;
+		uploadBackground();
+		uploadGreenCar();
+	}
 }
